399b8ac300b7ef4ac86f437cd81bf7fefa2b5df1
akka-tech <akkaabdlwahid3@gmail.com>
2025-09-21 13:08:02 +0200

    change

--- prodev-mobile-app-0x01/app/index.tsx
+++ prodev-mobile-app-0x01/app/index.tsx
@@ -3,9 +3,7 @@ import { Text, StyleSheet, View } from 'react-native';
 export default function HomeScreen() {
   return (
 <View>
-  <Text style={styles.largeText}>Typescript is great if you practice more</Text>
-  <Text style={styles.mediumText}>React Native provides you a single codebase for cross platforms</Text>
-  <Text style={styles.smallText}>ALX is awesome</Text>
+  <Text style={styles.largeText}>"Entry Screen - Awesome"</Text>
 </View>
   );
 }
